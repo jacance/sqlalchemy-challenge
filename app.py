3e1fff2b939d67632bada1bb60e4fd12b6d1ba01
Justin Acance <jacance@Justins-MacBook-Pro.local>
2020-12-20 00:18:10 -0800

    solved section 2 app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import dependencies
 import numpy as np
 
 import sqlalchemy
@@ -8,12 +9,13 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt
 
-
+# Create engine
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
+# Reflect the tables
 Base.prepare(engine, reflect=True)
 
 Station = Base.classes.station
@@ -21,6 +23,7 @@ Measurement = Base.classes.measurement
 
 app = Flask(__name__)
 
+# Home page
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
@@ -34,6 +37,7 @@ def home():
         f'/api/v1.0/<start>/<end>'
     )
 
+# Precipitation page
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
@@ -41,15 +45,14 @@ def precipitation():
 
     session.close()
 
-    all_prcp = []
-    for date, prcp in results:
-        prcp_dict = {}
-        prcp_dict["date"] = date
-        prcp_dict["prcp"] = prcp
-        all_prcp.append(prcp_dict)
-    
-    return jsonify(all_prcp)
+    # Dictionary with date as key and precipitation as value
+    prcp_dict = {
+        date: prcp
+        for date, prcp in results
+    }
+    return jsonify(prcp_dict)
 
+# Stations page
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
@@ -57,69 +60,73 @@ def stations():
 
     session.close()
 
+    # All stations
     all_stations = list(np.ravel(results))
     return jsonify(all_stations)
 
+# Page for temperature observations of most active station.
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
 
+    # Calculate one year before last day of data
     one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     one_year_ago
 
+    # Query last 12 months of temperature observation data for most active station
     active_12mo = session.query(Measurement.tobs).order_by(Measurement.tobs.asc()).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= one_year_ago).all()
 
+    session.close()
+
     active_12mo = list(np.ravel(active_12mo))
     return jsonify(active_12mo)
 
+# Page of minimum, average, and maximum temperature observations for a given day.
 @app.route("/api/v1.0/<start>")
 def start_date(start):
 
     session = Session(engine)
-    results = session.query(Measurement.date, Measurement.tobs).all()
+
+    # Query minimum, average, and maximum temperature observations for a given day.
+    results = list(np.ravel(session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()))
 
     session.close()
 
-    all_temp = []
-    for date, tobs in results:
-        temp_dict = {}
-        temp_dict["date"] = date
-        temp_dict["tobs"] = tobs
-        all_temp.append(temp_dict)
-    
-   
-    for x in all_temp:
-        search_term = x["date"]
-        if search_term == start:
-            return jsonify(session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
-                filter(Measurement.date >= start).all())
+    # All dates of measurement data
+    all_dates = list(np.ravel(session.query(Measurement.date).all()))
 
+    # Return results if start date entered by user is found in data. If not, return date not found.
+    for date in all_dates:
+        if start == date:
+            return jsonify(results)
+    return jsonify({"error": f"Date {start} found."}), 404
 
-    return jsonify({"error": "Date not found."}), 404
+# Page of minimum, average, and maximum temperature observations for a given start-end range.
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
 
-# @app.route("/api/v1.0/<start>/<end>")
-# def start_end(start_end):
+    session = Session(engine)
 
-#     session = Session(engine)
-#     results = session.query(Measurement.date, Measurement.tobs).all()
+    # Query minimum, average, and maximum temperature observations for a given start-end range.
+    results = list(np.ravel(session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()))
 
-#     session.close()
+    session.close()
 
-#     all_temp = []
-#     for date, tobs in results:
-#         temp_dict = {}
-#         temp_dict["date"] = date
-#         temp_dict["tobs"] = tobs
-#         all_temp.append(temp_dict)
-    
-#    canonicalized = real_name.replace("/", "Measurement.date <= ")
-#     for x in all_temp:
-#         search_term = x["date"]
-#         if search_term == start_end:
-#             return jsonify(session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
-#                 filter(Measurement.date >= start_end).all())
+    # All dates of measurement data
+    all_dates = list(np.ravel(session.query(Measurement.date).all()))
+
+    # Return results if start-end date range entered by user is found in data. If not, return date not found.
+    for date in all_dates:
+        if start == date:
+            for date in all_dates:
+                if end == date:
+                    return jsonify(results)
+
+    return jsonify({"error": f"Date {start} and/or {end} not found."}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
